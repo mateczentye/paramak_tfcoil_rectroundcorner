9498bedbf7bb1a3f39bcc717ffb484c678734692
Mate <mic24@kent.ac.uk>
2021-07-06 17:07:16 +0100

    trying to run codecov uploader from bash file2

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ pytest tests/ --cov=./ --cov-report=xml
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 
 chmod +x codecov
-./codecov -t ${{ secrets.CODECOV_TOKEN }}
+./codecov -t ${8cd3ec3f-7b8a-440b-bfec-7e61478c41e8}
