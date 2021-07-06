f1d4b11e4a73e5caae2914e2f015e4d3dda7274c
Mate <mic24@kent.ac.uk>
2021-07-06 17:26:48 +0100

    trying to run codecov uploader from bash file2

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ pytest tests/ --cov=./ --cov-report=xml
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 
 chmod +x codecov
-./codecov
+./codecov -t &{8cd3ec3f-7b8a-440b-bfec-7e61478c41e8} -u https://app.codecov.io/gh/mateczentye/paramak_tfcoil_rectroundcorner
